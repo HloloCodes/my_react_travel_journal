44f5ac0b0144d33e3250a232a518c3cc390c746b
Hlolo Modiane <Hlolocollection@gmail.com>
2025-06-03 20:47:11 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+
+import data from "./data"
+
+export default function App() {
+    
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry 
+            key={entry.id}
+            {...entry} // Spread operator to pass all properties of entry as props
+            />
+        )
+    })
+    
+    return (
+        <>
+            <Header />
+            <main className="container">
+                {entryElements}
+            </main>
+        </>
+    )
+}
